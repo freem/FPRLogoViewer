97daa2b68d840c19a577639370f1caa0f60b0c4d
freem <aj187@ajworld.net>
2017-03-28 12:10:25 -0500

    add credit to Jason Blackhart for checksum code in About Box (gg freem way to not do this for v2)

--- FPRLogoViewer/AboutBox.Designer.cs
+++ FPRLogoViewer/AboutBox.Designer.cs
@@ -30,14 +30,15 @@
 			this.labelVersion = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+			this.label3 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// buttonClose
 			// 
 			this.buttonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.buttonClose.Location = new System.Drawing.Point(12, 50);
+			this.buttonClose.Location = new System.Drawing.Point(12, 68);
 			this.buttonClose.Name = "buttonClose";
-			this.buttonClose.Size = new System.Drawing.Size(268, 23);
+			this.buttonClose.Size = new System.Drawing.Size(270, 23);
 			this.buttonClose.TabIndex = 0;
 			this.buttonClose.Text = "&OK";
 			this.buttonClose.UseVisualStyleBackColor = true;
@@ -55,7 +56,7 @@
 			// labelVersion
 			// 
 			this.labelVersion.AutoSize = true;
-			this.labelVersion.Location = new System.Drawing.Point(222, 9);
+			this.labelVersion.Location = new System.Drawing.Point(224, 9);
 			this.labelVersion.Name = "labelVersion";
 			this.labelVersion.Size = new System.Drawing.Size(58, 13);
 			this.labelVersion.TabIndex = 2;
@@ -66,6 +67,7 @@
 			// 
 			this.label2.AutoSize = true;
 			this.label2.Location = new System.Drawing.Point(9, 30);
+			this.label2.Margin = new System.Windows.Forms.Padding(3, 0, 4, 0);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(101, 13);
 			this.label2.TabIndex = 3;
@@ -74,7 +76,7 @@
 			// linkLabel1
 			// 
 			this.linkLabel1.AutoSize = true;
-			this.linkLabel1.Location = new System.Drawing.Point(154, 30);
+			this.linkLabel1.Location = new System.Drawing.Point(156, 30);
 			this.linkLabel1.Name = "linkLabel1";
 			this.linkLabel1.Size = new System.Drawing.Size(126, 13);
 			this.linkLabel1.TabIndex = 4;
@@ -83,13 +85,23 @@
 			this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.TopRight;
 			this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
 			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(9, 50);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(177, 13);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Checksum code by Jason Blackhart";
+			// 
 			// AboutBox
 			// 
 			this.AcceptButton = this.buttonClose;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.buttonClose;
-			this.ClientSize = new System.Drawing.Size(292, 79);
+			this.ClientSize = new System.Drawing.Size(294, 103);
+			this.Controls.Add(this.label3);
 			this.Controls.Add(this.linkLabel1);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.labelVersion);
@@ -113,5 +125,6 @@
 		private System.Windows.Forms.Label labelVersion;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.LinkLabel linkLabel1;
+		private System.Windows.Forms.Label label3;
 	}
 }
