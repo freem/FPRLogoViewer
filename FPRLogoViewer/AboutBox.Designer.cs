0f5badf067ffcb9eb25ff1866bf23182e2cde95b
freem <aj187@ajworld.net>
2017-03-28 12:18:54 -0500

    translate About Box to Japanese

--- FPRLogoViewer/AboutBox.Designer.cs
+++ FPRLogoViewer/AboutBox.Designer.cs
@@ -25,6 +25,7 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutBox));
 			this.buttonClose = new System.Windows.Forms.Button();
 			this.label1 = new System.Windows.Forms.Label();
 			this.labelVersion = new System.Windows.Forms.Label();
@@ -35,72 +36,45 @@
 			// 
 			// buttonClose
 			// 
+			resources.ApplyResources(this.buttonClose, "buttonClose");
 			this.buttonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.buttonClose.Location = new System.Drawing.Point(12, 68);
 			this.buttonClose.Name = "buttonClose";
-			this.buttonClose.Size = new System.Drawing.Size(270, 23);
-			this.buttonClose.TabIndex = 0;
-			this.buttonClose.Text = "&OK";
 			this.buttonClose.UseVisualStyleBackColor = true;
 			this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
 			// 
 			// label1
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(9, 9);
+			resources.ApplyResources(this.label1, "label1");
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(134, 13);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "Fire Pro Returns Logo Tool";
 			// 
 			// labelVersion
 			// 
-			this.labelVersion.AutoSize = true;
-			this.labelVersion.Location = new System.Drawing.Point(224, 9);
+			resources.ApplyResources(this.labelVersion, "labelVersion");
 			this.labelVersion.Name = "labelVersion";
-			this.labelVersion.Size = new System.Drawing.Size(58, 13);
-			this.labelVersion.TabIndex = 2;
-			this.labelVersion.Text = "versiontext";
-			this.labelVersion.TextAlign = System.Drawing.ContentAlignment.TopRight;
 			// 
 			// label2
 			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(9, 30);
-			this.label2.Margin = new System.Windows.Forms.Padding(3, 0, 4, 0);
+			resources.ApplyResources(this.label2, "label2");
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(101, 13);
-			this.label2.TabIndex = 3;
-			this.label2.Text = "Written by freem/AJ";
 			// 
 			// linkLabel1
 			// 
-			this.linkLabel1.AutoSize = true;
-			this.linkLabel1.Location = new System.Drawing.Point(156, 30);
+			resources.ApplyResources(this.linkLabel1, "linkLabel1");
 			this.linkLabel1.Name = "linkLabel1";
-			this.linkLabel1.Size = new System.Drawing.Size(126, 13);
-			this.linkLabel1.TabIndex = 4;
 			this.linkLabel1.TabStop = true;
-			this.linkLabel1.Text = "http://firepro.ajworld.net/";
-			this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.TopRight;
 			this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
 			// 
 			// label3
 			// 
-			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(9, 50);
+			resources.ApplyResources(this.label3, "label3");
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(177, 13);
-			this.label3.TabIndex = 5;
-			this.label3.Text = "Checksum code by Jason Blackhart";
 			// 
 			// AboutBox
 			// 
 			this.AcceptButton = this.buttonClose;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			resources.ApplyResources(this, "$this");
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.buttonClose;
-			this.ClientSize = new System.Drawing.Size(294, 103);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.linkLabel1);
 			this.Controls.Add(this.label2);
@@ -111,8 +85,6 @@
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "AboutBox";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "About Fire Pro Returns Logo Tool";
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
